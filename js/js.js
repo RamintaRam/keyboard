28a3434cefef37f26ac64d05a784050a0422a3bf
RamintaRam <raminta5@yahoo.com>
2017-08-01 10:05:12 +0300

    constanta's

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
-/**
- * Created by Raminta on 8/1/2017.
- */
+
+var FastTyping = function(){  // rasome is didziosios, nes tai yra objektas.
+// alert('working');
+//     initialize()
+
+    var CONST_STATE_REGISTER = "user_registration",
+        CONST_STATE_LEVEL_SELECTION = "choose_game_level",
+        CONST_STATE_GAME = "game_starts",
+        CONST_STATE_GAME_OVER = "game_over";
+}
